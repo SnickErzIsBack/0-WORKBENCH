1a60e724b60fc838259bb442417d667f09737607
SnickErzIsBack <thiemo.neumann@web.de>
2021-05-20 11:02:26 +0200

    2.b Parametrisierung

--- 05_Funktionen_01/script_01.js
+++ 05_Funktionen_01/script_01.js
@@ -1,7 +1,6 @@
 
 /***** Funktionen 01 *****/
-
-// 1. Kapselung von Codeblöcken
+//#region 1. Kapselung von Codeblöcken
 
 //Funktionsrumpf (body)
 function test()
@@ -11,18 +10,50 @@ function test()
 
 //Funktionsaufruf (call)
 //test();
+//#endregion
 
 /***** Funktionen 02a *****/
-
-//Parametrisierung
-//2.a Parametrisierung + Datenübergabe von INNEN
+//#region 2.a Parametrisierung + Datenübergabe von INNEN
 //mittels ReFactoring
 
-function ausgabeNamae()
+function ausgabeNamae1()
 {
     let firstNamae = "Deniz";
     console.log("Hallo " +  firstNamae + "!");
 }
 
-ausgabeNamae();
-console.log()
+/*
+function ausgabeNamae2()
+{
+    let firstNamae = "Frank";
+    console.log("Hallo " +  firstNamae + "!");
+}
+*/
+
+//ausgabeNamae1();
+//ausgabeNamae1();
+//console.log(firstNamae); // ERROR => local Variable, out of scope!
+//#endregion
+
+/***** Funktionen 02b *****/
+//#region //2.b Parametrisierung + Datenübergabe von AUSSEN
+
+function ausgabeNamaeParam(firstNamae) //Paramater
+    {
+        //wenn firstNamae leer, dann "Fremder"
+        if (firstNamae == undefined || firstNamae =="")
+            {
+                console.log("Hallo, Fremder!")
+            }
+        else
+            {
+                console.log("Hallo, " + firstNamae +"!");
+            }
+    }
+
+//ausgabeNamae1Param("Frank"); //call + Argument(e)
+//ausgabeNamae1Param(); //call + Argument(e)
+//prompt("Geben Sie bitte jetzt Ihren Namen ein: ")
+
+ausgabeNamaeParam(prompt("Geben Sie bitte jetzt Ihren Namen ein: "));
+//#endregion
