e61beb0e6054f17f21cebb7509329551ea0bda19
SnickErzIsBack <thiemo.neumann@web.de>
2021-05-20 14:45:02 +0200

    function VAR return

--- 05_Funktionen_01/script_01.js
+++ 05_Funktionen_01/script_01.js
@@ -36,9 +36,9 @@ function ausgabeNamae2()
 //#endregion
 
 /***** Funktionen 02b *****/
-//#region //2.b Parametrisierung + Datenübergabe von AUSSEN
+//#region 2.b Parametrisierung + Datenübergabe von AUSSEN
 
-function ausgabeNamaeParam(firstNamae) //Paramater
+/*function ausgabeNamaeParam(firstNamae) //Paramater
     {
         //wenn firstNamae leer, dann "Fremder"
         if (firstNamae == undefined || firstNamae =="")
@@ -50,7 +50,7 @@ function ausgabeNamaeParam(firstNamae) //Paramater
                 console.log("Hallo, " + firstNamae +"!");
             }
     }
-
+*/
 // ausgabeNamae1Param("Frank"); //call + Argument(e)
 // ausgabeNamae1Param("Dirk");
 // ausgabeNamae1Param(); 
@@ -60,8 +60,7 @@ function ausgabeNamaeParam(firstNamae) //Paramater
 /***** Funktionen 02c *****/
 //#region 2.c Mehrere Parameter
 
-/*
-function ausgabeNamaeParams(firstNamae,familyNamae) //Paramater
+/*function ausgabeNamaeParams(firstNamae,familyNamae) //Paramater
     {
         console.log("Hallo, " + firstNamae + " " + familyNamae + "!");
     }
@@ -70,9 +69,9 @@ function ausgabeNamaeParams(firstNamae,familyNamae) //Paramater
 //ausgabeNamaeParams(firstNamae,familyNamae);
 ausgabeNamaeParams(prompt("Bitte Vornamen eingeben:"),prompt("Bitte Nachnamen eingeben:"));
 */
-//#endregion
 
-/*      //wenn firstNamae leer, dann "Fremder"
+
+/*    //wenn firstNamae leer, dann "Fremder"
         if (firstNamae == undefined || firstNamae =="")
             {
                 console.log("Hallo, Fremder!")
@@ -82,23 +81,44 @@ ausgabeNamaeParams(prompt("Bitte Vornamen eingeben:"),prompt("Bitte Nachnamen ei
                 console.log("Hallo, " + firstNamae +"!");
             }
  */
+//#endregion
 
 /***** Funktionen 03a *****/
-//#region // 03a. Vorbereitung //Postulat: one function = one job (Uncle Bob)
+//#region 3a. Vorbereitung //Postulat: one function = one job (Uncle Bob)
 // SRP = Single Responsibility Principle
-            
-/*    function ausgabeNamaeParams2(firstNamae,familyNamae) //Paramater
-        {
-            // 1st job: string composing
-            let gap = " ";
-            let outputStr = "Hallo, " + firstNamae + gap + familyNamae + "!"
-            
-            // 2nd job: output
-            console.log(outputStr);
-        }
-    
-    ausgabeNamaeParams2(prompt("Bitte Vornamen eingeben:"),prompt("Bitte Nachnamen eingeben:"));
-*/
+/*            
+    function ausgabeNamaeParams2(firstNamae,familyNamae) //Paramater
+    {
+        // 1st job: string composing
+        let gap = " ";
+        let outputStr = "Hallo, " + firstNamae + gap + familyNamae + "!";
+        
+        // 2nd job: output
+        console.log(outputStr);
+    }
+*/    
+    //ausgabeNamaeParams2(prompt("Bitte Vornamen eingeben:"),prompt("Bitte Nachnamen eingeben:"));
+
 //#endregion
 
 /***** Funktionen 03b *****/
+//#region 3b. Funktionsaufruf aus anderer Funktion mit Variablenübergabe
+// 1st job: string composing
+
+output(getString("Drecks","Frank")); //Test
+function getString(firstNamae,familyNamae)
+{
+    let gap = " ";
+    let outputStr = "Hallo, " + firstNamae + gap + familyNamae + "!";
+    return outputStr; //data will be returned to call of function
+    console.log("huhu"); //code after return is unreachable, funtion has exited already
+}
+
+// 2nd job: output
+//  output("hi") // Unit-Test TDD => test-driven development
+
+function output(outputStr)
+{
+    console.log(outputStr);
+}
+//#endregion
