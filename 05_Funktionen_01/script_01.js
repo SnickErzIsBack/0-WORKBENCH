00ba22f5c58a3bdae94f6804a16d79bb9fcf0aed
SnickErzIsBack <thiemo.neumann@web.de>
2021-05-20 10:08:38 +0200

    1st upload

---
+++ 05_Funktionen_01/script_01.js
@@ -0,0 +1,28 @@
+
+/***** Funktionen 01 *****/
+
+// 1. Kapselung von Codeblöcken
+
+//Funktionsrumpf (body)
+function test()
+    {
+        console.log("Hallo Deniz");
+    }
+
+//Funktionsaufruf (call)
+//test();
+
+/***** Funktionen 02a *****/
+
+//Parametrisierung
+//2.a Parametrisierung + Datenübergabe von INNEN
+//mittels ReFactoring
+
+function ausgabeNamae()
+{
+    let firstNamae = "Deniz";
+    console.log("Hallo " +  firstNamae + "!");
+}
+
+ausgabeNamae();
+console.log()
