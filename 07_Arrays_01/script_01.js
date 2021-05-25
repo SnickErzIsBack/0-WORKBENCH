3eb95a01609ef285c6b9a612338116f1bfb38883
SnickErzIsBack <thiemo.neumann@web.de>
2021-05-25 12:44:09 +0200

    last entry

--- 07_Arrays_01/script_01.js
+++ 07_Arrays_01/script_01.js
@@ -2,9 +2,11 @@
 
 /***** 1.0. ARRAYS *****/
 
-//Array allows collections of data
+//Arrays allow collections of data
 
-/*  let arr;
+//#region
+/*
+    let arr;
     arr = new Array(); // constructor notation
     arr = []; // literal notation
     arr = [2,6,7,12]; // array with numerical data, length of 4
@@ -15,34 +17,100 @@
     ausgabe(arr.length);
     ausgabe(arr[0]); // index, ALWAYS starting with 0
 */
+//#endregion
 
 /* 1a. Loops (for-loop) */
 
-/* For-loop as repetitive structure
-for (let i = 0; i < 10; i++) {0
+/* For-loop as repetitive structure*/
+
+/*  for (let i = 0; i < 10; i++) {0
     //const element = array[index];
     ausgabe(i);    
-}
-*/
+}*/
 
 /* For-loop for the index of an array (Iteration)
+    let arr = ["Ich","bin","der","coole","Max","Mütze"]; 
+    for (let i = 0; i < arr.length; i++) {0
+        //const element = array[index];
+        ausgabe(arr[i]);    
+    }*/
 
-let arr = ["Ich","bin","der","coole","Max","Mütze"]; 
-for (let i = 0; i < arr.length; i++) {0
-    //const element = array[index];
-    ausgabe(arr[i]);    
-}
+/***** considering *****/
+
+/* 1. Einer Variablen kann ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: AUFSUMMIERUNG*/
+
+//#region - better: with a loop
+/* Zu "Fuss" - DRY !!!
+let a = 0; //Anfangswert
+ausgabe("inhalt von a: " + a);
+a = a + 1; // a = 0 + 1
+ausgabe("inhalt von a: " + a);
+...
 */
 
-/***** 02a. Funktionalität mit Array *****/
-// Criticism | considering:
+
+/*better: with a loop
+let a = 0; //Anfangswert
+for (let i = 0; i < 5; i++) {
+    ausgabe("in der loop a: " + a);
+    a += 1;  
+}
+
+ausgabe("nach der loop a: " + a);
+*/
+//#endregion
+
+/* 1a. Einer Variablen kann ih eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: KONKATENATION (Verkettung) eines Strings // TRANSPONIERUNG*/
+
+//#region 
+/*let str = ""; //Anfangswert
+let addStr = "Test";
+let gap = " ";
+let dot = ".";
+
+for (let i = 0; i < 5 ; i++){
+    str += addStr + gap;    
+}
+ausgabe(str += dot);    
+*/
+//#endregion
+
+/***** 02b. Funktionalität mit Array 2 *****/
+
+ausgabe(getSentenceArr2(["Ich","bin"])); //TEST
+ausgabe(getSentenceArr2(["Ich","bin","der","coole"])); //TEST
+ausgabe(getSentenceArr2(["Ich","bin","der","coole","Max","Mütze"])); //TEST
+function getSentenceArr2(arr) {
+    let str = "";
+    let gap = " ";
+    let dot = ".";
+    
+    for (let i = 0; i < arr.length ; i++){
+        if (i != arr.length-1) {
+            str += arr[i] + gap;        
+        } else {
+            str += arr[i] + dot;    
+        }
+    }
+    
+    
+    return str;
+}
+
+/***** 02a. Funktionalität mit Array 1*****/
+
+// critique | considering:
 // word1,word2,... word100 :: semantic structure
 // => TRANSFORMATION
 // arr[0],arr[1],... arr[100] :: numeric structure (0,1,2,3,... n)
 // => transformed a semantic issue to a numeric issue
 
 //ausgabe(getSentenceArr(["Ich","bin","Max","Mütze"])); // Test
-function getSentenceArr(arr) {
+/*function getSentenceArr(arr) {
     let gap = " ";
     let dot = ".";
 
@@ -51,12 +119,12 @@ function getSentenceArr(arr) {
               arr[2] + gap +
               arr[3] +
               dot;
-
+    
     return str;
 }
+*/
 
-
-/*** 01. Funktionalität mit Einzelparametern */
+/***** 01. Funktionalität mit Einzelparametern *****/
 // --> "Ich bin Max Mütze."
 //ausgabe(getSentence("Ich","bin","Max","Mütze")); // Test
 
@@ -80,3 +148,4 @@ function getSentenceArr(arr) {
 function ausgabe(outputStr) {
     console.log(outputStr);
 }
+;
