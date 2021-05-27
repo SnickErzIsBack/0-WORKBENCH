ca29d63c69951d9cb57dd16ec8d924dafe21b5cf
SnickErzIsBack <thiemo.neumann@web.de>
2021-05-27 14:55:08 +0200

    EVENT /Business Logic

--- 13_JS_Events/events.js
+++ 13_JS_Events/events.js
@@ -4,4 +4,54 @@ function ausgabe(outputStr) {
     console.log(outputStr);
 }
 
+/***** BTN *****/
+let btn = document.getElementById("trigBtn");
 
+//ausgabe(btn);
+
+/***** STATUS *****/
+let status = true;
+
+btn.addEventListener("click",toggleStatus);
+
+function toggleStatus() {
+    status = !status;
+    ausgabe(status);
+}
+
+
+/***** Tools *****/
+function test() {
+    let cond = (document.body.className == "day");
+    if (cond) {
+        // night-mode
+        classNameSwitcher("night");
+        switchBtnTxt("night");
+    } else {
+        // day-mode
+        classNameSwitcher("day");
+        switchBtnTxt("day");       
+    }
+}
+
+/***** Änderung in der View-Schicht *****/
+
+//Modul: Umschaltung Klasse | Test:
+
+//classNameSwitcher("night");
+//classNameSwitcher("day");
+
+function classNameSwitcher(modestr) {
+    document.body.className = modestr;
+    document.body.children[0].className = modestr;
+    document.body.children[1].className = modestr;
+}
+
+//Modul: Umschaltung BtnTxt | Test:
+
+//switchButtonTest("day");
+//switchButtonTest("night");
+
+function switchBtnTxt(modestr){
+    btn.innerHtml = modestr;
+}
