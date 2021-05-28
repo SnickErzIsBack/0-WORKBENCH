382f7ac12af834eecbc5a19d5191e11b048dc3bb
SnickErzIsBack <thiemo.neumann@web.de>
2021-05-28 09:16:44 +0200

    update view layer

--- 14_WebApp_Beverages/js/view_controller.js
+++ 14_WebApp_Beverages/js/view_controller.js
@@ -6,7 +6,7 @@
 	Check Daten :: 
     Btn. Trigger :: 
     Business-Logic (Alter --> Getränk) :: 
-    Bild austauschen :: 
+    Bild austauschen :: CHECK!
 */
 
 // Modul Ablaufsteuerung | Test:
@@ -23,7 +23,14 @@
 
 // Modul: Business-Logic (Mapping) | Test:
 
+
 // Modul: Bild aktualisieren | Test:
+//ausgabe(updateImg("cola"));
+function updateImg(imgName) {
+    let img = document.getElementById("bevImg");
+    img.src = "./bilder/" + imgName + ".jpg";
+    return imgName; //monitoring
+}
 
 //Modul: Konsolenausgabe --> Test:
 function ausgabe(outputStr) {
